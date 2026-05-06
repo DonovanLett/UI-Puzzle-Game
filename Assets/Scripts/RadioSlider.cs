0cf80aac88fa62f70c92d5bec9042ea3247c33f4
Donovan Lett <donovanlett85@gmail.com>
2026-05-06 13:09:27 -1000

    Plus and Minus sign added to Radio Slider

--- Assets/Scripts/RadioSlider.cs
+++ Assets/Scripts/RadioSlider.cs
@@ -35,7 +35,7 @@ public class RadioSlider : MonoBehaviour
     public void SetValue()
     {
         _correctValue = Random.Range(GetComponent<Slider>().minValue + _fadingDistance, GetComponent<Slider>().maxValue); // You plus _fadingDistance to minValue here, but you can remove it if you ever make it so that the initial value of the slider is randomzied as well.
-        _correctValue = Mathf.Ceil(_correctValue * 1000f) / 1000f;
+        _correctValue = Mathf.Round(_correctValue * 1000f) / 1000f; // Originally Mathf.Ceil(_correctValue * 1000f) / 1000f;
         _displayedText.text = _currentValue.ToString("000.000");
         _currentDistance = Mathf.Abs(_correctValue - _currentValue);
 
@@ -60,8 +60,25 @@ public class RadioSlider : MonoBehaviour
 
     public void ValueAdded(float additive)
     {
-        _currentValue = Mathf.Ceil(_currentValue * 1000f) / 1000f;
+        //Debug.Log("Current Value before Rounding: " + _currentValue);
+        //_currentValue = Mathf.Ceil(_currentValue * 1000f) / 1000f;
+        _currentValue = Mathf.Round(_currentValue * 1000f) / 1000f;
+        //Debug.Log("Current Value before Additive: " + _currentValue);
         _currentValue += additive;
+        //Debug.Log("Current Value after Additive: " + _currentValue);
+        // _currentValue = Mathf.Ceil(_currentValue * 1000f) / 1000f; // New
+        //Debug.Log("Slider Triggered; " + additive + " added to " + (_currentValue - additive) +  ". Should not be " + _currentValue);
+        if (_currentValue < GetComponent<Slider>().minValue)
+        {
+            _currentValue = GetComponent<Slider>().minValue;
+            Debug.Log("Min Value Hit");
+        }
+        else if (_currentValue > GetComponent<Slider>().maxValue)
+        {
+            _currentValue = GetComponent<Slider>().maxValue;
+            Debug.Log("Min Value Hit");
+        }
+
         GetComponent<Slider>().value = _currentValue;
         _displayedText.text = _currentValue.ToString("000.000");
         _currentDistance = Mathf.Abs(_correctValue - _currentValue);
